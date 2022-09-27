5e2e7e8992d5b34129709c3eb5759c91a1d3495b
Lars Liden <larsliden@gmail.com>
2022-09-27 16:12:51 -0700

    choose between plans with GPT
    If you set NumberOfResponses in OpenAIConfig to >1, will use GPT3 to pick between the plans

--- NaLaPla/OpenAIConfig.cs
+++ NaLaPla/OpenAIConfig.cs
@@ -3,7 +3,7 @@ namespace NaLaPla
 public class OpenAIConfig {
         public int MaxTokens = 500;
 
-        public int NumResponses = 2;
+        public int NumResponses = 1;
 
         public float Temperature =  0.2f;
     }
