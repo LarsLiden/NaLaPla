619f10ae9f89a481ded126fcfa4023ca481500c9
Lars Liden <larsliden@gmail.com>
2022-11-29 08:29:15 -0800

    bug fix and update gpt3
    Update gpt version and fix casing bug exposed by new version

--- NaLaPla/OpenAIConfig.cs
+++ NaLaPla/OpenAIConfig.cs
@@ -6,18 +6,20 @@ public class OpenAIConfig {
 
         public static int DefaultNumResponses = 4;
 
-        public static float DefaultTemperature =  0.3f;
-
-        public int MaxTokens = DefaultMaxTokens;
+        public int MaxTokens;
 
         // If >1 will produce multiple candidates and do an additional
         // call to GPT to vote for best candidate.  Additional experimentation
         // required to see how much value this adds
-        public int NumResponses = DefaultNumResponses;
+        public int NumResponses;
 
-        public float Temperature = DefaultTemperature;
+        public float Temperature;
 
-        public OpenAIConfig() {}
+        public OpenAIConfig() {
+            this.MaxTokens = DefaultMaxTokens;
+            this.NumResponses = DefaultNumResponses;
+            this.Temperature = RuntimeConfig.settings.gptTemperature; 
+        }
 
         public OpenAIConfig(int maxTokens, int numResponses, float temperature) {
             this.MaxTokens = maxTokens;
